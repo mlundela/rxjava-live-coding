0500587974b41a42f6374e990eed14edca360069
Mads André Lundeland <mads.lundeland@vimond.com>
2015-02-17 22:09:58 +0100

    Start

--- src/main/java/io/mlundela/rxjava/Main.java
+++ src/main/java/io/mlundela/rxjava/Main.java
@@ -1,8 +1,19 @@
 package io.mlundela.rxjava;
 
+import rx.Observable;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hi");
+    public static void main(String[] args) throws InterruptedException {
+
+        List<String> symbols = Arrays.asList("AMZN", "GOOG", "ORCL");
+
+        Observable<StockInfo> feed = StockServer.getFeed(symbols);
+        
+        
+
     }
 }
---
+++ src/main/java/io/mlundela/rxjava/StockServer.java
@@ -0,0 +1,12 @@
+package io.mlundela.rxjava;
+
+import rx.Observable;
+
+import java.util.List;
+
+public class StockServer {
+    
+    public static Observable<StockInfo> getFeed(List<String> symbols) {
+        throw new RuntimeException("Not implemented yet!");
+    }
+}
